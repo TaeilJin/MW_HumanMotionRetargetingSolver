fe641af928f5eb19b57e2085ea29d3162cb09453
TAE-IL JIN <jin219219@kaist.ac.kr>
2023-05-17 21:49:46 +0900

    download MW

--- LIBRARY_DOWNLOAD/init_library_vs2015_x64.bat
+++ LIBRARY_DOWNLOAD/init_library_vs2015_x64.bat
@@ -1,5 +1,3 @@
-wget http://motionlab.kaist.ac.kr/git_data/Libraries_vs2015_x64_TJ.7z
+wget https://github.com/TaeilJin/TaeilJin.github.io/raw/master/Libraries_vs2015_x64_TJ.7z
 
 7za x Libraries_vs2015_x64_TJ.7z -o"Libraries/"
-
-del Libraries_vs2015_x64_TJ.7z
